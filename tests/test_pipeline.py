00791708968647d660297f2a6b34901b270003d0
Onyesi-john <72194579+Onyesi-john@users.noreply.github.com>
2025-02-19 11:44:12 +0100

    Update test_pipeline.py

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+sys.path.insert(0, os.path.abspath(os.path.dirname(file)))
 import pytest
 import numpy as np
 from train import train_model  # Import your training function
